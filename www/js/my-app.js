fa519f3daf68a5f65caefdeebb0b8cbfd4911a04
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2017-10-06 16:51:58 +0530

    Final touches

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -31,6 +31,7 @@ $$(document).on('pageInit', function (e) {
         novisit++;
     }
     if (page.name == 'searchkhoj') {
+        alert("hi");
         var mySearchbar = myApp.searchbar('.searchbar', {
             searchList: '.list-block-search',
             searchIn: '.item-title'
